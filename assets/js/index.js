54ef5b599f32ea563d6967d76d2f0039189b37ec
Michele Capicchioni <capimichi@gmail.com>
2017-11-20 16:57:02 +0100

    Spostati file di Gigio nella cartella test/pie_chart

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,6 @@
 var chartContainer = d3.select("#chart-container");
 
+// Set the dimensions of the canvas / graph
 var margin = {
     top: 30,
     right: 20,
@@ -11,25 +12,51 @@ var width = 900 - margin.left - margin.right;
 
 var height = 600 - margin.top - margin.bottom;
 
+var tooltip = d3.select("body")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0)
+    .html('<i class="fa fa-times"></i> <br/><a href="#">Informazioni generiche per anno <span class="year"></span> </a><hr/><a href="#">Informazioni specifiche <span class="status"></span> per anno <span class="year"></span> </a><hr/><a href="#">Informazioni generiche <span class="status"></span> </a>');
+
+
 var x = d3.scale
     .linear()
     .range([0, width]);
+
 var y = d3.scale
     .linear()
     .range([height, 0]);
 
-var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(d3.format("d")).tickSubdivide(0);
-var yAxis = d3.svg.axis().scale(y).orient("left").ticks(20);
+var xAxis = d3
+    .svg
+    .axis()
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(d3.format("d"))
+    .tickSubdivide(0);
 
-var svg = chartContainer.append("svg")
+var yAxis = d3.svg.axis().scale(y)
+    .orient("left").ticks(20);
+
+var drawLine = d3.svg
+    .line()
+    .x(function (d) {
+        return x(d.x);
+    })
+    .y(function (d) {
+        return y(d.y);
+    });
+
+var svg = chartContainer
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
     .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// Get the data
-d3.csv("generale.csv", function (error, data) {
+d3.csv("data/index.csv", function (error, data) {
 
+    // Scale the range of the data
     x.domain([
         d3.min(data, function (d) {
             return d.date;
@@ -38,18 +65,32 @@ d3.csv("generale.csv", function (error, data) {
             return d.date;
         })
     ]);
+
     y.domain([0, 100]);
 
 
-    // Add the valueline path.
+    var occupatiLineData = [];
+    var nonOccupatiLineData = [];
+
+    data.forEach(function (p1, p2, p3) {
+        occupatiLineData.push({
+            x: p1.date,
+            y: p1.occupati
+        });
+        nonOccupatiLineData.push({
+            x: p1.date,
+            y: p1.non_occupati
+        });
+    });
+
     svg.append("path")
         .attr("class", "line occupati")
-        .attr("d", occupatiLine(data));
+        .attr("d", drawLine(occupatiLineData));
 
     // Add the valueline path.
     svg.append("path")
         .attr("class", "line non_occupati")
-        .attr("d", nonOccupatiLine(data));
+        .attr("d", drawLine(nonOccupatiLineData));
 
     // Add the X Axis
     svg.append("g")
@@ -62,63 +103,46 @@ d3.csv("generale.csv", function (error, data) {
         .attr("class", "y axis")
         .call(yAxis);
 
-    svg.selectAll("dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "circle occupati")
-        .attr("r", 5)
-        .attr("cx", function (d) {
-            return x(d.date);
-        })
-        .attr("cy", function (d) {
-            return y(d.occupati);
-        })
-        .on("mouseover", function (d) {
-            var dot = d3.select(this);
-            dot.classed("hover", true);
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html("<strong>Anno</strong><div>" + d.date + "</div><hr/><strong>Occupati</strong><div>" + d.occupati + "%</div>")
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY) + "px");
-        })
-        .on("mouseout", function (d) {
-            var dot = d3.select(this);
-            dot.classed("hover", false);
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+    var statuses = Object.keys(data[0]);
 
-    svg.selectAll("dot")
-        .data(data)
-        .enter()
-        .append("circle")
-        .attr("class", "circle non_occupati")
-        .attr("r", 5)
-        .attr("cx", function (d) {
-            return x(d.date);
-        })
-        .attr("cy", function (d) {
-            return y(d.non_occupati);
-        })
-        .on("mouseover", function (d) {
-            var dot = d3.select(this);
-            dot.classed("hover", true);
-            tooltip.transition()
-                .duration(200)
-                .style("opacity", .9);
-            tooltip.html("<strong>Anno</strong><div>" + d.date + "</div><hr/><strong>Non occupati</strong><div>" + d.non_occupati + "%</div>")
-                .style("left", (d3.event.pageX) + "px")
-                .style("top", (d3.event.pageY) + "px");
-        })
-        .on("mouseout", function (d) {
-            var dot = d3.select(this);
-            dot.classed("hover", false);
-            tooltip.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+    statuses.shift();
+
+    statuses.forEach(function (p1, p2, p3) {
+        svg.selectAll("dot")
+            .data(data)
+            .enter()
+            .append("circle")
+            .attr("class", "circle " + p1)
+            .attr("r", 5)
+            .attr("cx", function (d) {
+                return x(d.date);
+            })
+            .attr("cy", function (d) {
+                return y(d[p1]);
+            })
+            .on("click", function (d) {
+                console.log(d);
+                tooltip.transition()
+                    .duration(200)
+                    .style("opacity", .9)
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY) + "px");
+                tooltip.selectAll(".year").text(d.date);
+                tooltip.selectAll(".status").text(p1);
+            })
+            .on("mousehover", function (d) {
+                var dot = d3.select(this);
+                dot.classed("hover", true);
+            })
+            .on("mouseout", function (d) {
+                var dot = d3.select(this);
+                dot.classed("hover", false);
+            });
+        //     tooltip.transition()
+        //         .duration(500)
+        //         .style("opacity", 0);
+        // });
+    });
+
+
 });
